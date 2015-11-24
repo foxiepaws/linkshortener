2df07eb3ec5680b955e162e2c77fdd3ac70bcbc2
Allie Theze <fox@foxiepa.ws>
2015-11-24 14:44:36 -0500

    more changes
      * changed from lug to slug.
      * removed the extra "," from link
      * moved from DateTime to EpochTime
      * relationship to hits has been added

--- lib/models/link.rb
+++ lib/models/link.rb
@@ -4,14 +4,14 @@ module Linkshortener
   class Link
     include DataMapper::Resource
     
-    property :lug, String, :key => true
-    property :link, URI, :required => true,
+    property :slug, String, :key => true
+    property :link, URI, :required => true
     property :trigger, Boolean, :required => true, :lazy => false,
              :default => true
     property :triggerContent, Text
-    property :added, DateTime
+    property :added, EpochTime
     property :ipaddr, IPAddress, :required => true
     # TODO: add stats.
-    #has n, :hits
+    has n, :hits
   end 
 end 
