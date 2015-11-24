888ad028a99a1ef08405bb8400a8466e48353bdf
Allie Theze <fox@foxiepa.ws>
2015-11-24 14:38:16 -0500

    fixed the Hit model
      included thed DataMapper::Resource class
      added a primary key
      the referrer is now a URI type string
      Switched to epochtime from datetime
      useragent element is now of type text.

--- lib/models/hit.rb
+++ lib/models/hit.rb
@@ -2,10 +2,12 @@
 
 module Linkshortener
   class Hit
-    property :referrer, String  # Where did the person access from (if available)
-    property :time, DateTime    # what time did the access happen
-    property :followed, Boolean, # was the link followed
-             :default => true
+    include DataMapper::Resource
+    property :id, Serial
+    property :referrer, URI  # Where did the person access from (if available)
+    property :time, EpochTime    # what time did the access happen
+    property :user_agent, Text
+    
     belongs_to :link
   end
 end
