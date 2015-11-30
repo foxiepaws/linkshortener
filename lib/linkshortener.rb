f44f04c9dd0e715b639ed957fca278fb9021fee4
Allie Theze <fox@foxiepa.ws>
2015-11-29 22:21:48 -0500

    bring up to date with local work.

--- lib/linkshortener.rb
+++ lib/linkshortener.rb
@@ -7,6 +7,21 @@ require 'ipaddr'
 require File.join File.dirname(__FILE__),'models/link'
 require File.join File.dirname(__FILE__),'models/hit'
 
+
+
+# TODO: RESTful API middleware
+# TODO: configuration file
+# TODO: normalise locals for templates.
+# TODO: stats pages
+# TODO: admin panel
+# TODO: remove debug code.
+# TODO: i18n related code for strings.
+# TODO: unit tests...
+# TODO: BETTER COMMENTS
+# TODO: reports table
+# TODO: administration related tables.
+# TODO: ban code (before block.)
+
 module Linkshortener
   class App < Sinatra::Base
 
@@ -25,7 +40,7 @@ module Linkshortener
       Liquid::LocalFileSystem.new(settings.views)
 
 
-
+    
     settings.datastores.each do |store,dsn|
       DataMapper::setup(store,dsn)
     end
@@ -39,6 +54,7 @@ module Linkshortener
     helpers do 
       def stats(link)
         dnt = (env['HTTP_DNT'] =~ /^1$/) ? true : false
+
         print "stats routine: dnt on? " + dnt.to_s +
               " tracking anyway? " + settings.always_track_hitcount.
                                      to_s + "\n"
@@ -66,15 +82,17 @@ module Linkshortener
     
        
     get '/' do
-      # show a pretty index page
-      liquid :index, :locals => { :sitename => settings.site_name }
+      liquid :index, :locals => { :sitename => settings.site_name}
     end
     
     get '/create' do
       liquid :create, :locals => { :page_title => "Add Link",
                                    :sitename => settings.site_name }
     end
-    
+
+
+    # TODO: See if there is a more efficient way of writing this...
+    #       84 lines is means SOMETHING can be shortened or written better...
     post '/create' do
 
       # a bit of a nasty hack with regards to flow control.
@@ -82,7 +100,7 @@ module Linkshortener
         slug = rand(36**settings.default_slug_length).to_s(36)
         return slug
       end
-
+      @slug = ""
       customSlug     = params['customSlug']
       trigger        = (params['tw'] =~ /^on$/) ? true : false
       triggerContent = params['triggerContent']
@@ -101,8 +119,9 @@ module Linkshortener
                 
               )
               success = true
+              @slug = customSlug
             rescue DataMapper::SaveFailureError => e
-              slug = customSlug
+              @slug = customSlug
               success = false 
               errormsg = "DEBUG: " + e.resource.errors.inspect.to_s
             rescue DataObjects::IntegrityError => e
@@ -111,19 +130,19 @@ module Linkshortener
               else
                 errormsg = "DEBUG: e.to_s"
               end
-              slug = customSlug
+              @slug = customSlug
               success = false
             end
         else
           success = false
-          errormsg =            "The custom slug provided is invalid. Only the ASCII characters A through Z, a though z, 0 through 9, and - are allowed."
+          errormsg = "The custom slug provided is invalid. Only the ASCII characters A through Z, a though z, 0 through 9, and - are allowed."
         end
       else
-        slug = genSlug()
+        @slug = genSlug()
         
         begin
           res = Link.create(
-            :slug => slug,
+            :slug => @slug,
             :trigger => trigger,
             :triggerContent => triggerContent,
             :link => link,
@@ -143,15 +162,20 @@ module Linkshortener
       end
       
       if success
-       return call env.merge("PATH_INFO" => "/stats/#{slug}")
-      else
-              liquid :create,
-                     :locals => { :page_title => "Add Link",
-                                  :sitename => settings.site_name,
-                                  :errormsg => errormsg }
+        liquid :create,
+               :locals => { :page_title => "Add Link",
+                            :sitename => settings.site_name,
+                            :slug => @slug,
+                            :success => true }
 
+      else
+        liquid :create,
+               :locals => { :page_title => "Add Link",
+                            :sitename => settings.site_name,
+                            :slug => @slug,
+                            :errormsg => errormsg }
+        
       end                  
-      #                          }
     end
     
     get '/:lug' do |lug|
@@ -171,6 +195,22 @@ module Linkshortener
         pass
       end 
     end
+
+    # TODO: move to API call.
+    get '/existcheck/:lug' do |lug|
+      content_type "application/json"
+      begin 
+        link = Link.get!(lug)
+        "true"
+      rescue DataMapper::ObjectNotFoundError
+        "false"
+      end
+    end
+    
+    get '/report/:lug' do |lug|
+      # TODO: make reporting code.
+      pass
+    end
     
     get '/preview/:lug' do |lug|
       begin
@@ -182,10 +222,11 @@ module Linkshortener
                                       :triggerContent => link.triggerContent
                                     }
       rescue DataMapper::ObjectNotFoundError
-        "other fail"
+        pass
       end
     end
 
+    # TODO: remove these endpoints when removing debug code.
     get '/previewtest' do 
       liquid :preview, :locals => { :sitename => settings.site_name,
                            :link => "/",
@@ -199,6 +240,7 @@ module Linkshortener
                            :triggerContent => "NSFW"
                          }
     end
+    # endTODO 
       
     get '/stats' do
       "Links: " + Link.count.to_s +
